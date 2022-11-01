988332050fe87e8f1149d8e1f5694a93443cc0cd
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-01 21:08:33 +0100

    finished task 'todo-list-render-list'

---
+++ dom/todo-list-render-list/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
+const list = document.querySelector('.list');
+
+/**
+ * @param {object[]} tasksList
+ * @return {undefined}
+ */
+const renderTasks = tasksList => {
+  const tasksListTemplate = tasksList.reduce((template, { text, done }) => template + (
+    `<li class="${done ? 'list__item list__item_done' : 'list__item' }">
+        <input type="checkbox" class="list__item-checkbox" />
+        ${text}
+      </li>`
+  ), '');
+
+  list.innerHTML = tasksListTemplate;
+};
+
+renderTasks(tasks);
