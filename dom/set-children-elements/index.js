5176eeb5db2b3b2cbc30cf8d868128da7db3ca25
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-01 23:50:46 +0100

    finished task 'update-text-content'

--- dom/set-children-elements/index.js
+++ dom/set-children-elements/index.js
@@ -1,6 +1,5 @@
 export function setButton(buttonText) {
   document.querySelector('body').innerHTML = buttonText;
-  document.querySelector('body').textContent += buttonText;
 }
 
 setButton('<button>button text</button>');
