5f9224b7fed446061098b5cde26aebdcd223c3ad
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-01 21:24:13 +0100

    finished task 'todo-list-render-list'

---
+++ dom/searching-for-item-on-page/index.js
@@ -0,0 +1,11 @@
+export function getTitleElement() {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
+}
+
+export function getInputElement() {
+  const inputElem = document.querySelector('[type="text"]');
+  console.dir(inputElem);
+  return inputElem;
+}
