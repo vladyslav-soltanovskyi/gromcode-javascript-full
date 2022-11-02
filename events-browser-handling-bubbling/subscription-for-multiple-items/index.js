e5e00ba82f2208297cb8fe4ad0a71ecfb29332e0
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-02 16:47:26 +0100

    finished task 'handling-event-for-checkbox-change'

--- events-browser-handling-bubbling/subscription-for-multiple-items/index.js
+++ events-browser-handling-bubbling/subscription-for-multiple-items/index.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelector('.pagination__page');
+const buttons = document.querySelectorAll('.pagination__page');
 
 const handleClick = (event) => {
   console.log(event.target.dataset.pageNumber);
