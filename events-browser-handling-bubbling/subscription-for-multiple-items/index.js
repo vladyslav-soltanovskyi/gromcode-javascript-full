03f84976a6eb840cd209fde2b809a759de512dcc
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-02 16:38:04 +0100

    finished task 'subscription-for-multiple-items'

---
+++ events-browser-handling-bubbling/subscription-for-multiple-items/index.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelector('.pagination__page');
+
+const handleClick = (event) => {
+  console.log(event.target.dataset.pageNumber);
+}
+
+buttons.forEach(button => button.addEventListener('click', handleClick));
