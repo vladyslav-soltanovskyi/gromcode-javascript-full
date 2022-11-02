99775c636bd371b7c18d63390d2bf3b7d689317c
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-02 18:54:04 +0100

    finished task 'work-with-form'

---
+++ events-forms-delegation/work-with-form/index.js
@@ -0,0 +1,45 @@
+const loginForm = document.querySelector('.login-form');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const errorFields = document.querySelectorAll('.error-text');
+
+const validateFields = ({ email, password }) => {
+  let errors = {};
+
+  if (email === '') {
+    errors.email = 'Required';
+  } else if (!email.includes('@')) {
+    errors.email = 'Should be an email';
+  }
+
+  if (password === '') {
+    errors.password = 'Required';
+  }
+
+  return errors;
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const formData = Object.fromEntries(new FormData(e.target));
+  const errors = validateFields(formData);
+
+  if (!Object.keys(errors).length) {
+    alert(JSON.stringify(formData))
+  }
+}
+
+const setErrors = () => {
+  const errors = validateFields({ email: email.value, password: password.value });
+  errorFields.forEach(field => field.textContent = '');
+  Object.entries(errors).forEach(([name, textError]) => {
+    const fieldError = document.querySelector(`#${name}`).nextElementSibling;
+    fieldError.textContent = textError;
+  })
+}
+
+setErrors();
+
+email.addEventListener('input', setErrors);
+password.addEventListener('input', setErrors);
+loginForm.addEventListener('submit', handleSubmit)
