2f516f07d300eacc19d9ca24ad1e4fe6dce7d103
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-02 21:41:47 +0100

    finished task 'todo-list-create-update-task'

--- events-forms-delegation/todo-list-create-update-task/index.js
+++ events-forms-delegation/todo-list-create-update-task/index.js
@@ -37,16 +37,18 @@ const createTask = () => {
 function renderTasks(tasksList) {
   const prevCheckboxes = list.querySelectorAll('.list__item-checkbox');
   prevCheckboxes.forEach(checkbox => checkbox.removeEventListener('click', toggleDoneStatus));
+  list.innerHTML = '';
 
   const tasksElements = tasksList
     .sort((a, b) => a.done - b.done)
-    .map(({ text, done }) => {
+    .map(({ text, done, id }) => {
       const listItemElem = document.createElement('li');
       listItemElem.classList.add('list__item');
       const checkbox = document.createElement('input');
       checkbox.setAttribute('type', 'checkbox');
       checkbox.checked = done;
       checkbox.classList.add('list__item-checkbox');
+      checkbox.dataset.taskId = id;
       if (done) {
         listItemElem.classList.add('list__item_done');
       }
