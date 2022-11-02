ecffc72a0671e80c9135b87215e7890fc336116c
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-02 15:21:29 +0100

    finished task 'input-search'

---
+++ events-browser-handling-bubbling/input-search/index.js
@@ -0,0 +1,4 @@
+const searchBtn = document.querySelector('.search__btn');
+const searchInput = document.querySelector('.search__input');
+
+searchBtn.addEventListener('click', () => console.log(searchInput.value));
