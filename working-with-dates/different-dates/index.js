b66459a8221c6348e9b97a532d8526a7a1dcb681
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-03 17:22:58 +0100

    finished task 'different-dates'

---
+++ working-with-dates/different-dates/index.js
@@ -0,0 +1,12 @@
+const getDiff = (startDate, endDate) => {
+  const startTime = Date.parse(startDate);
+  const endTime = Date.parse(endDate);
+  const diffTime = startTime > endTime ? startTime - endTime : endTime - startTime;
+
+  const seconds = Math.floor((diffTime / 1000) % 60);  
+  const minutes = Math.floor((diffTime / (1000 * 60)) % 60);  
+  const hours = Math.floor((diffTime / (1000 * 60 * 60)) % 24);  
+  const days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
