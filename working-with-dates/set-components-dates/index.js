4f0b83a7a8d91d786f21c676c2d1f4018ae8a585
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-03 16:24:23 +0100

    finished task 'set-components-dates'

--- working-with-dates/set-components-dates/index.js
+++ working-with-dates/set-components-dates/index.js
@@ -1,13 +1,9 @@
 const arrDaysOfWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St']
 
-export const daysOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
   const day = new Date(date).getDate();
   const dateInFuture = new Date(date).setDate(day + days);
   const indexDayOfWeek = new Date(dateInFuture).getDay();
   
   return arrDaysOfWeek[indexDayOfWeek];
 }
-
-const result = daysOfWeek(new Date(2019, 0, 1), 14);
-
-console.log(result);
