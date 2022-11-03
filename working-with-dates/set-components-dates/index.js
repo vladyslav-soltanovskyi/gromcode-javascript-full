2128420c1ac3060f9d8865c468545003e7d7b1f2
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-03 16:22:42 +0100

    finished task 'todo-list-create-update-task'

--- working-with-dates/set-components-dates/index.js
+++ working-with-dates/set-components-dates/index.js
@@ -1,6 +1,6 @@
 const arrDaysOfWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St']
 
-const daysOfWeek = (date, days) => {
+export const daysOfWeek = (date, days) => {
   const day = new Date(date).getDate();
   const dateInFuture = new Date(date).setDate(day + days);
   const indexDayOfWeek = new Date(dateInFuture).getDay();
