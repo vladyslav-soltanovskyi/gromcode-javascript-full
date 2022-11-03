a22d70f1aa7d233997cdc72f235622540752039f
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-03 16:42:53 +0100

    finished task 'time-greenwich'

--- working-with-dates/time-greenwich/index.js
+++ working-with-dates/time-greenwich/index.js
@@ -1,3 +1,3 @@
 export const getGreenwichTime = (date) => {
-  return new Intl.DateTimeFormat('ua-UA', { hour: 'numeric', minute: 'numeric' }).format(date); 
+  return new Intl.DateTimeFormat('ua-UA', { hour: 'numeric', minute: 'numeric' }).format(Date.UTC(date)); 
 }
