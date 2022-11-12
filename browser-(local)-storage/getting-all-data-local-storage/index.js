29b092cbe21a0039c98957904ca1fe8a17447855
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-12 13:27:14 +0100

    finished task 'getting-all-data-local-storage'

--- browser-(local)-storage/getting-all-data-local-storage/index.js
+++ browser-(local)-storage/getting-all-data-local-storage/index.js
@@ -1,7 +1,15 @@
 export const getLocalStorageData = () => (
   Object.entries(localStorage)
-    .reduce((acc, [key, value]) => ({
-      ...acc,
-      [key]: JSON.parse(value)
-    }))
+    .reduce((acc, [key, value]) => {
+      let newValue;
+      try {
+        newValue = JSON.parse(value)
+      } catch(e) {
+        newValue = value
+      }
+      return {
+        ...acc,
+        [key]: newValue
+      }
+    })
 )
