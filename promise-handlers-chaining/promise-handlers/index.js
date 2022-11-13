b5d762494b21296cb91f83f96488076aa47eacce
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-13 18:38:40 +0100

    finished task 'promise-handlers'

---
+++ promise-handlers-chaining/promise-handlers/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @param {string} userId
+ * @return {promise}
+ */
+export const requestUserData = userId => {
+  // put your code here
+  return new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new Error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          userId,
+          email: `${userId}@example.com`
+        });
+      }, 1000);
+    }
+  })
+};
