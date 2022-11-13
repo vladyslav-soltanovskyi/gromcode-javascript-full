9e4480eb26490d34680b3c10e59619afc56ac816
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-13 19:02:17 +0100

    finished task 'chain-of-promises'

--- promise-handlers-chaining/chain-of-promises/index.js
+++ promise-handlers-chaining/chain-of-promises/index.js
@@ -1,12 +1,12 @@
 export const asyncCalculator = (numbr) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       console.log(`Intial value: ${numbr}`);  
       resolve(numbr)
     }, 500);
   })
   .then(value => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         console.log(`Squared value: ${value**2}`);  
         resolve(value**2)
