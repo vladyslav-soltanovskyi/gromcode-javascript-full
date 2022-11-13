68e98119d551d575758ef73884cec745ef56b2d0
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-13 16:38:45 +0100

    finished task 'write-with-interval'

--- asynchronous-code/wrtie-with-interval/index.js
+++ asynchronous-code/wrtie-with-interval/index.js
@@ -17,7 +17,3 @@ export const pinger = (count, period) => {
   ping();
   intervalId = setInterval(ping, period)
 };
-
-// examples
-pinger(5, 100); // makes 5 writes with 100 ms interval
-pinger(7, 150); // makes 7 writes with 1500 ms interval
