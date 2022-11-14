2eb87873e715016afcb246e8ad2705bcfc56918c
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-14 23:57:26 +0100

    finished task 'race-of-promises'

---
+++ promise-static-methods/race-of-promises/index.js
@@ -0,0 +1,20 @@
+const random = (min, max) => Math.random() * (max - min) + min;
+
+const fakeApi = (source) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        userData: { name: 'Tom', age: 17 },
+        source
+      });
+    }, random(1, 3));
+  });
+}
+
+export const getUserASAP = (userId) => (
+  Promise.race([
+    fakeApi(`https://server.com/us/${userId}`).then(response => response.json()),
+    fakeApi(`https://server.com/eu/${userId}`).then(response => response.json()),
+    fakeApi(`https://server.com/au/${userId}`).then(response => response.json())
+  ])
+)
