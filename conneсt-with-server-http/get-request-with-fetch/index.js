3eb9318c1994fcd9e1b96db98e75867f513921e2
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-16 00:22:33 +0100

    finished task 'get-request-with-fetch'

---
+++ conneсt-with-server-http/get-request-with-fetch/index.js
@@ -0,0 +1,27 @@
+const userAvatar = document.querySelector('.user__avatar');
+const userName = document.querySelector('.user__name');
+const userLocation = document.querySelector('.user__location');
+const nameFormInput = document.querySelector('.name-form__input');
+const nameFormBtn = document.querySelector('.name-form__btn');
+
+const fetchUserData = (userName) => {
+  return fetch(`https://api.github.com/users/${userName}`)
+    .then(res => res.json());
+}
+
+const renderUserData = ({ avatar_url, name, location }) => {
+  userAvatar.src = avatar_url;
+  userName.innerHTML = name;
+  userLocation.innerHTML = location;
+}
+
+const onSearchUser = () => {
+  const userName = nameFormInput.value;
+  if (!userName) {
+    return;
+  }
+  fetchUserData(userName)
+    .then(userData => renderUserData(userData));
+}
+
+nameFormBtn.addEventListener('click', onSearchUser);
