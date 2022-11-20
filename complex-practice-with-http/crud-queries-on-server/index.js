9c6b66ccddc16f65acdd7ee0c627912c16a10e2d
Vladyslav Soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-20 12:57:46 +0300

    finished task 'crud-queries-on-server'

--- complex-practice-with-http/crud-queries-on-server/index.js
+++ complex-practice-with-http/crud-queries-on-server/index.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://61d8e2cfe6744d0017ba8cdc.mockapi.io/users';
+const baseUrl = 'https://5e5af5eb37d2eh00167913f52.mockapi.io/api/v1/users';
 
 const request = (url, { method = 'GET', body = {}, ...rest } = {}) => {
   const headers = {
@@ -35,37 +35,3 @@ export function deleteUser(userId) {
 export function updateUser(userId, userData) {
   return request(`/${userId}`, { method: "PUT", body: userData });
 }
-
-getUsersList().then(users => {
-  console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-});
-
-getUserById('2').then(userData => {
-  console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-});
-
-const newUserData = {
-  email: 'cool@email.com',
-  firstName: 'Iron',
-  lastName: 'Man',
-  age: 42,
-};
-
-createUser(newUserData).then(() => {
-  console.log('User created');
-});
-
-const updatedUserData = {
-  email: 'new@email.com',
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 17,
-};
-
-updateUser('1', updatedUserData).then(() => {
-  console.log('User updated');
-});
-
-deleteUser('2').then(() => {
-  console.log('User updated');
-});
