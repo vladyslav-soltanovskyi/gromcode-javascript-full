05ed885fd97b4740bce15a0eea7fa49c20e54f37
Vladyslav Soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-21 19:03:27 +0300

    finished task 'crud-queries-on-server'

--- complex-practice-with-http/crud-queries-on-server/index.js
+++ complex-practice-with-http/crud-queries-on-server/index.js
@@ -1,27 +1,28 @@
 const baseUrl = 'https://61d8e2cfe6744d0017ba8cdc.mockapi.io/users';
 
-const request = (url, { method = 'GET', body = {}, ...rest } = {}) => {
-  const headers = {
-    'Content-Type': 'application/json;charset=utf-8',
-    ...rest.headers
-  };
-
-  const options = (method === 'GET') ? (
-  { method, ...rest, headers }
-  ) : (
-  { method, body: JSON.stringify(body), ...rest, headers }
-  )
-
-  return fetch(`${baseUrl}${url}`, options)
-    .then(res => res.json());
-}
+const request = (url, options) => (
+  fetch(`${baseUrl}${url}`, options)
+    .then(res => res.json())
+);
 
 export const getUsersList = () => request('');
 
 export const getUserById = (userId) => request(`/${userId}`);
 
-export const createUser = (userData) => request('', { method: "POST", body: userData });
+export const createUser = (userData) => request('', {
+  method: "POST",
+  body: JSON.stringify(userData),
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  }
+});
 
 export const deleteUser = (userId) => request(`/${userId}`, { method: "DELETE" });
 
-export const updateUser = (userId, userData) => request(`/${userId}`, { method: "PUT", body: userData });
+export const updateUser = (userId, userData) => request(`/${userId}`, {
+  method: "PUT",
+  body: JSON.stringify(userData),
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  }
+});
