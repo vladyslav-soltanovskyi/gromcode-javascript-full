8918674bfa845968a29473d1dbbba64c260b7861
Vladyslav Soltanovskyi <vladsoltanovskih@gmail.com>
2022-11-22 17:35:41 +0300

    finished task 'get-user-async-await'

--- promise-advanced-async-await/get-user-async-await/index.js
+++ promise-advanced-async-await/get-user-async-await/index.js
@@ -4,16 +4,12 @@ export const fetchUser = async (userId) => {
   try {
     const res = await fetch(`https://api.github.com/users/${userId}`);
 
-    if (res.status === 404) {
-        return null;
-    }
-
     if (!res.ok) {
-        throw new Error('Failed to get user data');
+        return null;
     }
 
     return (await res.json());
   } catch(err) {
-    console.log(err.message);
+    throw new Error('Failed to get user data');
   }
 };
